3e931f95ba816e4bf4691ed9ca6521343ea2b34f
Tim Gonschorek <tim.gonschorek@ovgu.de>
2020-12-09 21:42:21 +0100

    initial code commit

---
+++ ctrl/src/ctrl/kinematics/direct/fw_kinematics.cpp
@@ -0,0 +1,13 @@
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+#include "fw_kinematics.h"
+
+
+SixDPos* FwKinematics::get_fw_kinematics(Configuration *_cfg)
+{
+    //TODO: IMPLEMENT the computation of the forward kinematics and derive position and euler angles. Keep in mind your
+    //                definition of the rotations and whether you are working in deg or rad as well as in m or cm.
+
+    return new SixDPos(1.757, 0.0, 1.91, 0, M_PI, 0);
+}
---
+++ ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
@@ -0,0 +1,22 @@
+#define _USE_MATH_DEFINES
+
+#include "inverse_kinematics.h"
+#include <math.h>
+vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
+{
+    //TODO: IMPLEMENT Compute the inverse kinematics for a given position
+
+    //prepare the result vector for the configurations
+    // you should call your inverse kinematics functions here!
+    vector<Configuration*>* solutions = new vector<Configuration*>();
+    solutions->push_back(new Configuration({0,0,1,0,0,0}));
+    solutions->push_back(new Configuration({1/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({2/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({3/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({4/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({5/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({6/8 * M_PI,0,1,0,0,0}));
+    solutions->push_back(new Configuration({7/8 * M_PI,0,1,0,0,0}));
+
+    return solutions;
+}
