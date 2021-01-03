22dd7da9169cf4ac39b4a40641efea7351b5fb16
Mats <mats.naujoks@st.ovgu.de>
2021-01-03 16:51:19 +0100

    resolved errors in inverse kinematics

--- ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
+++ ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
@@ -115,14 +115,225 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
     else if(wcp[0]<0 && wcp[1]<0)
     {
         phi1=180-atan(wcp[1]/wcp[0]);
+        if(d1>m && wcp[2]>n && -175<phi1<175)                                                                               //calculating all the forward cases
+        {
+            double dpx = d1 - m;                                                                                            //calculating the x and y components of the
+            double dpy = wcp[2] - n;                                                                                        //distance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                                                                    //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_f_u = -1 * (alpha1 + alpha2);                                                                       //for forward elbow up
+            double phi2_f_d = -1 * (alpha2 - alpha1);                                                                       //for forward elbow down
+
+            double phi3_f_u = 360 - beta - asin(b / d2) - 90;                                                            //for forward elbow up
+            double phi3_f_d = beta - asin(b / d2) - 90;                                                                  //for forward elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_f_u < -5) {
+                    if (-120 < phi3_f_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_f_u;
+                        solution[2] = phi3_f_u;
+                    }
+
+                    if (-140 < phi2_f_d < -5) {
+                        if (-120 < phi3_f_d < 168) {
+                            solution[0] = phi1;
+                            solution[1] = phi2_f_d;
+                            solution[2] = phi3_f_d;
+                        }
+                    }
+                }
+
+            }
+        }
+        else if(d1 < m && -175 < phi1 < 175)                              //calculating all the backward cases
+        {
+            double dpx = m - d1;                                  //calculating the x and y components of the
+            double dpy = wcp[2] - n;                              //distnance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                  //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_b_u = -1 * (180 - (alpha1 + alpha2));          //for backwards elbow up
+            double phi2_b_d = -1 * (180 - (alpha2 - alpha1));          //for backwards elbow down
+
+            double phi3_b_u = 270 - beta - asin(b / d2);              //for backwards elbow up
+            double phi3_b_d = -1 * (90 - (beta - asin(b / d2)));        //for backwards elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_b_u < -5) {
+                    if (-120 < phi3_b_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_u;
+                        solution[2] = phi3_b_u;
+                    }
+                }
+                if (-140 < phi2_b_d < -5) {
+                    if (-120 < phi3_b_d < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_d;
+                        solution[2] = phi3_b_d;
+                    }
+                }
+            }
+        }
+
     }
     else if(wcp[0]>0 && wcp[1]<0)
     {
         phi1=atan(wcp[1]/wcp[0]);
+        if(d1>m && wcp[2]>n && -175<phi1<175)                                                                               //calculating all the forward cases
+        {
+            double dpx = d1 - m;                                                                                            //calculating the x and y components of the
+            double dpy = wcp[2] - n;                                                                                        //distance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                                                                    //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_f_u = -1 * (alpha1 + alpha2);                                                                       //for forward elbow up
+            double phi2_f_d = -1 * (alpha2 - alpha1);                                                                       //for forward elbow down
+
+            double phi3_f_u = 360 - beta - asin(b / d2) - 90;                                                            //for forward elbow up
+            double phi3_f_d = beta - asin(b / d2) - 90;                                                                  //for forward elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_f_u < -5) {
+                    if (-120 < phi3_f_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_f_u;
+                        solution[2] = phi3_f_u;
+                    }
+
+                    if (-140 < phi2_f_d < -5) {
+                        if (-120 < phi3_f_d < 168) {
+                            solution[0] = phi1;
+                            solution[1] = phi2_f_d;
+                            solution[2] = phi3_f_d;
+                        }
+                    }
+                }
+
+            }
+        }
+        else if(d1 < m && -175 < phi1 < 175)                              //calculating all the backward cases
+        {
+            double dpx = m - d1;                                  //calculating the x and y components of the
+            double dpy = wcp[2] - n;                              //distnance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                  //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_b_u = -1 * (180 - (alpha1 + alpha2));          //for backwards elbow up
+            double phi2_b_d = -1 * (180 - (alpha2 - alpha1));          //for backwards elbow down
+
+            double phi3_b_u = 270 - beta - asin(b / d2);              //for backwards elbow up
+            double phi3_b_d = -1 * (90 - (beta - asin(b / d2)));        //for backwards elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_b_u < -5) {
+                    if (-120 < phi3_b_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_u;
+                        solution[2] = phi3_b_u;
+                    }
+                }
+                if (-140 < phi2_b_d < -5) {
+                    if (-120 < phi3_b_d < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_d;
+                        solution[2] = phi3_b_d;
+                    }
+                }
+            }
+        }
     }
     else if(wcp[0]<0 && wcp[1]>0)
     {
         phi1=180-atan(wcp[1]/-wcp[0]);
+        if(d1>m && wcp[2]>n && -175<phi1<175)                                                                               //calculating all the forward cases
+        {
+            double dpx = d1 - m;                                                                                            //calculating the x and y components of the
+            double dpy = wcp[2] - n;                                                                                        //distance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                                                                    //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_f_u = -1 * (alpha1 + alpha2);                                                                       //for forward elbow up
+            double phi2_f_d = -1 * (alpha2 - alpha1);                                                                       //for forward elbow down
+
+            double phi3_f_u = 360 - beta - asin(b / d2) - 90;                                                            //for forward elbow up
+            double phi3_f_d = beta - asin(b / d2) - 90;                                                                  //for forward elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_f_u < -5) {
+                    if (-120 < phi3_f_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_f_u;
+                        solution[2] = phi3_f_u;
+                    }
+
+                    if (-140 < phi2_f_d < -5) {
+                        if (-120 < phi3_f_d < 168) {
+                            solution[0] = phi1;
+                            solution[1] = phi2_f_d;
+                            solution[2] = phi3_f_d;
+                        }
+                    }
+                }
+
+            }
+        }
+        else if(d1 < m && -175 < phi1 < 175)                              //calculating all the backward cases
+        {
+            double dpx = m - d1;                                  //calculating the x and y components of the
+            double dpy = wcp[2] - n;                              //distnance between the second joint and wcp
+
+            double d3 = sqrt(dpx * dpx + dpy + dpy);                  //direct distance between joint and wcp
+            double d2 = sqrt(o * o + b * b);
+            double beta = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (-2 * a * d2));
+            double alpha1 = asin(sinh(beta) * (d2 / d3));
+            double alpha2 = asin(dpy / d3);
+
+            double phi2_b_u = -1 * (180 - (alpha1 + alpha2));          //for backwards elbow up
+            double phi2_b_d = -1 * (180 - (alpha2 - alpha1));          //for backwards elbow down
+
+            double phi3_b_u = 270 - beta - asin(b / d2);              //for backwards elbow up
+            double phi3_b_d = -1 * (90 - (beta - asin(b / d2)));        //for backwards elbow down
+
+            if (-185 < phi1 < 185) {
+                if (-140 < phi2_b_u < -5) {
+                    if (-120 < phi3_b_u < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_u;
+                        solution[2] = phi3_b_u;
+                    }
+                }
+                if (-140 < phi2_b_d < -5) {
+                    if (-120 < phi3_b_d < 168) {
+                        solution[0] = phi1;
+                        solution[1] = phi2_b_d;
+                        solution[2] = phi3_b_d;
+                    }
+                }
+            }
+        }
     }
 
         //Special cases
