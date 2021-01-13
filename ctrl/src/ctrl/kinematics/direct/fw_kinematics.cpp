2c4d42fff732ef0bceac35cd0e2d9e8994087b7f
Lukas Bostelmann-Arp <Lukas.Bostelmann-Arp@st.ovgu.de>
2021-01-13 16:36:55 +0100

    Reformatted fw_kinematics, no other changes.

--- ctrl/src/ctrl/kinematics/direct/fw_kinematics.cpp
+++ ctrl/src/ctrl/kinematics/direct/fw_kinematics.cpp
@@ -5,14 +5,7 @@
 #include "fw_kinematics.h"
 
 
-SixDPos* FwKinematics::get_fw_kinematics(Configuration *_cfg)
-{
-    //TODO: IMPLEMENT the computation of the forward kinematics and derive position and euler angles. Keep in mind your
-    //                definition of the rotations and whether you are working in deg or rad as well as in m or cm.
-
-    // TODO: Look into whether it is possible to precalculate the final
-    //       transformation matrix for better performance and precision.
-
+SixDPos *FwKinematics::get_fw_kinematics(Configuration *_cfg) {
     // Euler angle test
     //TMatrix mat(0.8212351, -0.353276, 0.5694953, 0, 0.1512832, 0.9758426, -0.1576219, 0, -0.5501693, 0.2155997, 0.8067406, 0, 0, 0, 0, 1);
     //vector<double> angles = get_euler_angles(mat);
@@ -29,13 +22,13 @@ SixDPos* FwKinematics::get_fw_kinematics(Configuration *_cfg)
 
     // These are the Denavit-Hartenberg parameters for our robot
     double dh_table[7][4] = {
-            {      0,    M_PI,    0,   645},
-            {      0,  M_PI_2,  330,     0},
-            {      0,       0, 1150,     0},
-            {-M_PI_2,  M_PI_2,  115,     0},
-            {      0, -M_PI_2,    0, -1220},
-            {      0,  M_PI_2,    0,     0},
-            {   M_PI,    M_PI,    0,  -215}
+            {0,       M_PI,   0,    645},
+            {0,       M_PI_2, 330,  0},
+            {0, 0,            1150, 0},
+            {-M_PI_2, M_PI_2, 115,  0},
+            {0, -M_PI_2,      0,    -1220},
+            {0,       M_PI_2, 0,    0},
+            {M_PI,    M_PI,   0,    -215}
     };
 
     // Transformation matrix from the base coordinate system
@@ -50,14 +43,14 @@ SixDPos* FwKinematics::get_fw_kinematics(Configuration *_cfg)
 
     // Create and multiply the individual transformation matrices
     // from one joint to the next one.
-    for(int i = 0; i < 6; i++) {
-      TMatrix next = create_single_t_matrix(
-              dh_table[i+1][0] + (*_cfg)[i],
-              dh_table[i+1][1],
-              dh_table[i+1][2],
-              dh_table[i+1][3]
-      );
-      A = A.multiply(next);
+    for (int i = 0; i < 6; i++) {
+        TMatrix next = create_single_t_matrix(
+                dh_table[i + 1][0] + (*_cfg)[i],
+                dh_table[i + 1][1],
+                dh_table[i + 1][2],
+                dh_table[i + 1][3]
+        );
+        A = A.multiply(next);
     }
 
     // A now contains the final transformation matrix denoting the
@@ -85,52 +78,50 @@ SixDPos* FwKinematics::get_fw_kinematics(Configuration *_cfg)
     );
 }
 
-TMatrix FwKinematics::create_single_t_matrix(double theta, double alpha, double r, double d)
-{
-  double t00 = cos(theta);
-  double t01 = -sin(theta)*cos(alpha);
-  double t02 = sin(theta)*sin(alpha);
-  double t03 = r*cos(theta);
-  double t10 = sin(theta);
-  double t11 = cos(theta)*cos(alpha);
-  double t12 = -cos(theta)*sin(alpha);
-  double t13 = r*sin(theta);
-  double t20 = 0;
-  double t21 = sin(alpha);
-  double t22 = cos(alpha);
-  double t23 = d;
-  double t30 = 0;
-  double t31 = 0;
-  double t32 = 0;
-  double t33 = 1;
+TMatrix FwKinematics::create_single_t_matrix(double theta, double alpha, double r, double d) {
+    double t00 = cos(theta);
+    double t01 = -sin(theta) * cos(alpha);
+    double t02 = sin(theta) * sin(alpha);
+    double t03 = r * cos(theta);
+    double t10 = sin(theta);
+    double t11 = cos(theta) * cos(alpha);
+    double t12 = -cos(theta) * sin(alpha);
+    double t13 = r * sin(theta);
+    double t20 = 0;
+    double t21 = sin(alpha);
+    double t22 = cos(alpha);
+    double t23 = d;
+    double t30 = 0;
+    double t31 = 0;
+    double t32 = 0;
+    double t33 = 1;
 
-  return {
-          t00, t01, t02, t03,
-          t10, t11, t12, t13,
-          t20, t21, t22, t23,
-          t30, t31, t32, t33
-  };
+    return {
+            t00, t01, t02, t03,
+            t10, t11, t12, t13,
+            t20, t21, t22, t23,
+            t30, t31, t32, t33
+    };
 }
 
-vector<double> FwKinematics::get_euler_angles(TMatrix& tMatrix)
-{
-  double* data = tMatrix.get_matrix();
-  double phi, theta, psi;
+vector<double> FwKinematics::get_euler_angles(TMatrix &tMatrix) {
+    double *data = tMatrix.get_matrix();
+    double phi, theta, psi;
 
-  auto T = [data](int row, int col) {
-    return data[(row-1)*4+col-1];
-  };
+    auto T = [data](int row, int col) {
+        return data[(row - 1) * 4 + col - 1];
+    };
 
-  double epsilon = 0.00174532925;    // 0.1 degrees
+    double epsilon = 0.00174532925;    // 0.1 degrees
 
-  if (abs(T(1,1)) < epsilon && abs(T(2,1)) < epsilon) {
-    phi     = asin(-T(1,2) );
-    theta   = - T(3, 1) * M_PI * 0.5;
-    psi     = 0;
-  } else {
-    phi     = atan2(T(2,1), T(1,1));
-    theta   = atan2(-T(3,1), sqrt(pow(T(3,2), 2) + pow(T(3,3), 2)));
-    psi     = atan2(T(3,2), T(3,3));
-  }
-  return vector<double>({phi, theta, psi});
+    if (abs(T(1, 1)) < epsilon && abs(T(2, 1)) < epsilon) {
+        phi = asin(-T(1, 2));
+        theta = -T(3, 1) * M_PI * 0.5;
+        psi = 0;
+    } else {
+        phi = atan2(T(2, 1), T(1, 1));
+        theta = atan2(-T(3, 1), sqrt(pow(T(3, 2), 2) + pow(T(3, 3), 2)));
+        psi = atan2(T(3, 2), T(3, 3));
+    }
+    return vector<double>({phi, theta, psi});
 }
--- ctrl/src/ctrl/kinematics/direct/fw_kinematics.h
+++ ctrl/src/ctrl/kinematics/direct/fw_kinematics.h
@@ -7,16 +7,22 @@
 #include <Configuration.h>
 
 /**
- * This class is intended to handle the direct kinematic computation from a given configuration
+ * This class is intended to handle the direct kinematic computation from a given configuration.
+ * It makes use of Roll, Pitch, Yaw Euler angles:
+ * Extrinsic:
+ *  Yaw around z, Pitch around y', Roll around z''
+ * Intrinsic:
+ *  Roll around z, Pitch around y, Yaw around x
  */
 class FwKinematics {
 public:
     /**
      * Computes the forward kinematic from a given {@Configuration}
+     *
      * @param _cfg {@Configuration} to compute the forward kinematic from
      * @return computed {@ref SixDPos}
      */
-    SixDPos* get_fw_kinematics(Configuration* _cfg);
+    SixDPos *get_fw_kinematics(Configuration *_cfg);
 
 private:
 
@@ -39,7 +45,7 @@ private:
     * @param Homogeneous transformation matrix
     * @return Set of Euler Angles denoting Roll, Pitch, and Yaw [rad]
     */
-    vector<double> get_euler_angles(TMatrix& tMatrix);
+    vector<double> get_euler_angles(TMatrix &tMatrix);
 
 };
 
