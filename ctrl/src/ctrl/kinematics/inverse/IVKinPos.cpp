51cfaad79b3132528f722ef32eb25a1b659bad17
Maximilian Rötzer <redster@web.de>
2021-01-15 13:47:11 +0100

    IVKinPos Better Code Style and unterstandable TODO Fix Bugs and Special cases

--- ctrl/src/ctrl/kinematics/inverse/IVKinPos.cpp
+++ ctrl/src/ctrl/kinematics/inverse/IVKinPos.cpp
@@ -5,58 +5,81 @@
 IVKinPos::IVKinPos()
 {
 }
-
-IVKinPos::IVKinPos(double _x, double _y, double _z)
+IVKinPos::~IVKinPos()
 {
-	P.x = _x;
-	P.y = _y;
-	P.z = _z;
 }
 
-std::vector<std::array<double, 3>*>* IVKinPos::get_IVKinPos()
+
+std::vector<std::array<double, 3>*>* IVKinPos::get_IVKinPos(SixDPos* _pos)
 {
 	double phi1;
-	//Standard 1
-	if ((P.x > 0 && P.y > 0))
+	//Wrist Point
+	_pos = new SixDPos(0.0, 0.0, 1.5, 0.0, 1.0, 1.0);
+	TMatrix m_transEndeffector2Wrist = TMatrix(_pos->get_A(), _pos->get_B(), _pos->get_C(), _pos->get_X(), _pos->get_Y(), _pos->get_Z());
+	Position wristPoint;
+	wristPoint.x = _pos->get_X() - d_6 * m_transEndeffector2Wrist.get(0, 2);
+	wristPoint.y = _pos->get_Y() - d_6 * m_transEndeffector2Wrist.get(1, 2);
+	wristPoint.z = _pos->get_Z() - d_6 * m_transEndeffector2Wrist.get(2, 2);
+
+	std::cout << "wristPoint " << wristPoint.x << " " << wristPoint.y << " " << wristPoint.z << endl;
+	double maximumDistance = sqrt(wristPoint.x  *wristPoint.x + wristPoint.y*wristPoint.y + wristPoint.z * wristPoint.z);
+
+	if (wristPoint.x > -marginSingularity && wristPoint.x < marginSingularity &&
+		wristPoint.y > -marginSingularity && wristPoint.y < marginSingularity)
 	{
-		phi1 = rad2Deg(-atan(P.y / P.x));
-		return standardCase(phi1, P);
+		phi1 = 0.0;
+		std::cout << "Overhead Singularity" << endl;
+	}
+	else
+		phi1 = rad2Deg(atan2(wristPoint.y, wristPoint.x));
+	
+	std::cout << "phi1 " << phi1 << endl;
+	return standardCase_phi1(phi1, wristPoint);
+	////1 Quadrant
+	//if ((wristPoint.x > 0 && wristPoint.y > 0))
+	//{
+	//	std::cout << "1 Quadrant " << std::endl;
+	//	//eigentlich + phi1 aber in formeln -
+	//	phi1 = rad2Deg(atan(wristPoint.y / wristPoint.x));
+	//	return standardCase_phi1(phi1, wristPoint);
+	//	
+	//}
+	//// 4. Quadrant
+	//if (wristPoint.x > 0 && wristPoint.y < 0)
+	//{
+	//	std::cout << "4 Quadrant " << std::endl;
+	//	phi1 = rad2Deg(atan(wristPoint.y / wristPoint.x));
+	//	return standardCase_phi1(phi1, wristPoint);
+	//}
+	//// 3. Quadrant
+	//if (wristPoint.x < 0 && wristPoint.y < 0)
+	//{
+	//	std::cout << "3 Quadrant " << std::endl;
+	//	phi1 = rad2Deg(M_PI - atan(wristPoint.y / wristPoint.x));
+	//	return standardCase_phi1(phi1, wristPoint);
+	//}
+	////4. Quadrant
+	//if (wristPoint.x < 0 && wristPoint.y > 0)
+	//{
+	//	std::cout << "4 Quadrant " << std::endl;
+	//	phi1 = rad2Deg(M_PI - atan(wristPoint.y / -wristPoint.x));
+	//	return standardCase_phi1(phi1, wristPoint);
+	//}
+	// Special Case 1
+	if (wristPoint.x == 0 && wristPoint.y > 0)
+	{
+		std::cout << "Special Case 1 " << std::endl;
 		
 	}
-	if (P.x > 0 && P.y < 0)
-	{
-		phi1 = rad2Deg(atan(P.y / P.x));
-		return standardCase(phi1, P);
-	}
-	//Standard 2
-	if (P.x < 0 && P.y < 0)
-	{
-		phi1 = rad2Deg(M_PI - atan(P.y / P.x));
-		return standardCase(phi1, P);
-	}
-	// Standard 3
-	if (P.x < 0 && P.y > 0)
-	{
-		phi1 = rad2Deg(M_PI - atan(P.y / -P.x));
-		return standardCase(phi1, P);
-	}
-	// Special Case 1
-	if (P.x == 0 && P.y > 0)
-	{
-		return specialcase1(P);
-	}
 	// Special Case 2
-	if (P.x == 0 && P.y < 0)
+	if (wristPoint.x == 0 && wristPoint.y < 0)
 	{
-		return specialcase2(P);
+		std::cout << "Special Case 2 " << std::endl;
+
 	}
 
 }
 
-
-IVKinPos::~IVKinPos()
-{	
-}
 
 double IVKinPos::deg2Rad(double _deg)
 {
@@ -68,366 +91,174 @@ double IVKinPos::rad2Deg(double _rad)
 	return _rad * (180.0 / M_PI);
 }
 
-std::array<double, 3>* IVKinPos::checkLimits(double _phi1, std::array<double, 5>* _solution)
+void IVKinPos::checkLimits(double _phi1, std::array<double, 4>* _solution, std::vector<std::array<double, 3>*>* _ans)
 {
-	static std::array<double, 3> ans;
+	/*phi2_backward_upward,
+		phi2_backward_downward,
+		phi3_backward_upward,
+		phi3_backward_downward,
+		phi3_backward_downward_1*/
+	std::cout << "checkLimits " << endl;
+	std::array<double, 3>* config = new std::array<double, 3>();
 	if (-185.0 < _phi1 && _phi1 < (185.0))
 	{
 		if ((-140.0) < _solution->at(0) && _solution->at(0) < (-5.0)) //
 		{
-			if ((-120.0) < _solution->at(2) && _solution->at(2) <(168))
+			if ((-120.0) < _solution->at(2) && _solution->at(2) < (168))
 			{
-				ans = { _phi1,_solution->at(0),_solution->at(2) };
-				return &ans;
+				std::cout << "Downwards" << endl;
+				config = new std::array<double, 3>{ _phi1, _solution->at(0), _solution->at(2) };
+				_ans->push_back(config);
 			}
-				
+
 		}
 		if ((-140.0) < _solution->at(1) && _solution->at(1) < (-5.0)) //
 		{
 			if ((-120.0) < _solution->at(3) && _solution->at(3) < (168))
 			{
-				ans = { _phi1,_solution->at(1),_solution->at(3) };
-				return &ans;
-			}
-	
-			if ((-120.0) < _solution->at(4) && _solution->at(4) < (168))
-			{
-				ans = { _phi1,_solution->at(1),_solution->at(4) };
-				return &ans;
+				std::cout << "Upwards" << endl;
+				config = new std::array<double, 3>{ _phi1,_solution->at(1),_solution->at(3) };
+				_ans->push_back(config);
 			}
 		}
-	
+
 	}
-	return nullptr;
 }
 
-std::vector<std::array<double, 3>*>* IVKinPos::standardCase(double _phi1, Position _P)
+
+
+
+std::vector<std::array<double, 3>*>* IVKinPos::standardCase_phi1(double _phi1, Position _wristPoint)
 {
-	double d1 = std::sqrt(P.x + P.x + P.y * P.y);
-	double px_dash, py_dash;
-	std::array<double, 3>* sol;
-	std::array<double, 5>* solution;
-	static std::vector<std::array<double, 3>*> ans;
-	if ((-185.0) < _phi1 && _phi1 < (-175.0))
-	{
-		return checkphi1_case1(_phi1, _P);
-	}
-	if((175.0) < _phi1 &&  _phi1 < (185.0))
-	{
-		return checkphi1_case2(_phi1, _P);
-	}
+	std::array<double, 4>* backAndDownwardSolutions;
+	std::vector<std::array<double, 3>*>* sol;
+	std::vector<std::array<double, 3>*>* ans = new std::vector<std::array<double, 3>*>();
+	double d1 = std::sqrt(_wristPoint.x * _wristPoint.x + _wristPoint.y * _wristPoint.y);
 	if ((-175.0) <= _phi1 && _phi1 <= (175.0))
 	{
-		if (d1 > m && P.z > n)//?
-		{
-			std::cout << "forward case " << std::endl;
-			px_dash = d1 - m;
-			py_dash = P.z - n;
-			solution = forward_calc(_phi1, px_dash, py_dash);
-			std::array<double, 3>* sol = checkLimits(_phi1, solution);
-			if (sol != nullptr)
-				ans.push_back(sol);
-			sol = otherCase1(_phi1, d1, P);
-			if (sol != nullptr)
-				ans.push_back(sol);
-			sol = otherCase2(_phi1, d1, P);
-			if (sol != nullptr)
-				ans.push_back(sol);
-		}
-		if (d1 < n)
-		{
-			std::cout << "backward case " << std::endl;
-			px_dash = m - d1;
-			py_dash = P.z;
-			solution = backward_calc(_phi1, px_dash, py_dash);
-
-			std::array<double, 3>* sol = checkLimits(_phi1, solution);
-			if (sol != nullptr)
-				ans.push_back(sol);
-			sol = otherCase1(_phi1, d1, P);
-			if (sol != nullptr)
-				ans.push_back(sol);
-			sol = otherCase2(_phi1, d1, P);
-			if (sol != nullptr)
-				ans.push_back(sol);
-		}
-		
-	}
-
-	return &ans;
-}
-
-std::array<double, 3>* IVKinPos::otherCase1(double _phi1, double _d1, Position _P)
-{
-	double px_dash = _d1 - m;//? +
-	double py_dash = _P.z - n;
+		//double  backward_ph1 = wristPoint.y > 0 ? (180 - _phi1) : -(180 + _phi1);
 	
-	double phi1_new1 = _phi1 + 180.0;
-	std::array<double, 5>* solution = backward_calc(phi1_new1, px_dash, py_dash);
-	return checkLimits(phi1_new1, solution);
-}
+	}
+	if ((-185.0) < _phi1 && _phi1 < (-175.0))
+	{
+		std::cout << "Phi1 is between 175 to 185 or between 175 to 185 we have 2 forward solution and only one backwards solution" << endl;
+		
+	}
 
-std::array<double, 3>* IVKinPos::otherCase2(double _phi1, double _d1, Position _P)
-{
-	double px_dash = _d1 - m;
-	double py_dash = _P.z - n;
 
-	double phi1_new2 = _phi1 - 180.0;
-	std::array<double, 5>* solution = backward_calc(phi1_new2, px_dash, py_dash);
-	return checkLimits(phi1_new2, solution);
-}
 
-std::vector<std::array<double, 3>*>* IVKinPos::checkphi1_case1(double _phi1, Position _P)
-{
-	double d1 = sqrt(P.x*P.x + P.y * P.y);
-	double px_dash, py_dash, phi1_new;
-	std::array<double, 3>* sol;
-	static std::vector<std::array<double, 3>*> ans;
+	backAndDownwardSolutions = forward_calc(d1,_wristPoint);
+	checkLimits(_phi1, backAndDownwardSolutions, ans);
+
+	double phi1_backward = rad2Deg(atan2(-_wristPoint.y, -_wristPoint.x));
+	backAndDownwardSolutions = backward_calc(d1, _wristPoint);
+	checkLimits(phi1_backward, backAndDownwardSolutions, ans);
 	
-	if (d1 > m && P.z > n)
-	{
-		px_dash = d1 - m;
-		py_dash = P.z - n;
-		
-		sol = checkLimits(_phi1,forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-		
-		phi1_new =_phi1 + 360.0;
-		sol = checkLimits(phi1_new, forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
+	
+	
 
-		phi1_new = phi1_new + 180.0;//?
-		px_dash = d1 + m;
-		sol = checkLimits(phi1_new, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		
-	}
-	if (d1 < m)
-	{
-		px_dash = m - d1;
-		py_dash = P.z - n;
-
-		sol = checkLimits(_phi1, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = _phi1 + 360.0;
-		sol = checkLimits(phi1_new, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = phi1_new +180.0;//?
-		px_dash = d1 + m;
-		sol = checkLimits(phi1_new, forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		
-	}
-
-	return &ans;
-}
-
-std::vector<std::array<double, 3>*>* IVKinPos::checkphi1_case2(double _phi1, Position _P)
-{
-
-	double d1 = sqrt(P.x*P.x + P.y * P.y);
-	double px_dash, py_dash, phi1_new;
-	std::array<double, 3>* sol;
-	static std::vector<std::array<double, 3>*> ans;
-
-	if (d1 > m && P.z > n)
-	{
-		px_dash = d1 - m;
-		py_dash = P.z - n;
-
-		sol = checkLimits(_phi1, forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = _phi1 - 360.0;
-		sol = checkLimits(phi1_new, forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = phi1_new - 180;//?
-		px_dash = d1 + m;
-		sol = checkLimits(phi1_new, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-	}
-	if (d1 < m)
-	{
-		px_dash = m - d1;
-		py_dash = P.z - n;
-
-		sol = checkLimits(_phi1, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = _phi1 - 360.0;
-		sol = checkLimits(phi1_new, backward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1_new = phi1_new - 180.0;//?
-		px_dash = d1 + m;
-		sol = checkLimits(phi1_new, forward_calc(_phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-	}
-
-	return &ans;
-}
-
-std::vector<std::array<double, 3>*>* IVKinPos::specialcase1(Position _P)
-{
-	double d1, phi1, px_dash, py_dash;
-	std::array<double, 3>* sol;
-	static std::vector<std::array<double, 3>*> ans;
-	if (P.y > m)
-	{
-		 d1= P.y;
-	     phi1 = -90.0;
-		 px_dash = P.y - m;
-		 py_dash = P.z - n;
-
-		 sol = checkLimits(phi1,forward_calc(phi1, px_dash, py_dash));
-		 if (sol != nullptr)
-			 ans.push_back(sol);
-
-		 phi1 = 90.0;
-		 px_dash = P.y + m;
-
-		 sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		 if (sol != nullptr)
-			 ans.push_back(sol);
-		
-		 return &ans;
-	}
-	if (P.y < m)
-	{
-		d1 = P.y;
-		phi1 = -90.0;
-		px_dash = m - P.y;
-		py_dash = P.z - n;
-
-		sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1 = 90.0;
-		px_dash = P.y + m;
-
-		sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		return &ans;
-	}
-	return nullptr;
-}
-
-std::vector<std::array<double, 3>*>* IVKinPos::specialcase2(Position _P)
-{
-	double d1, phi1, px_dash, py_dash;
-	std::array<double, 3>* sol;
-	std::vector<std::array<double, 3>*> ans;
-	if (P.y > m)
-	{
-		d1 = P.y;
-		phi1 = 90.0;
-		px_dash = P.y - m;
-		py_dash = P.z - n;
-
-		sol = checkLimits(phi1, forward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1 = -90.0;
-		px_dash = P.y + m;
-
-		sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		return &ans;
-	}
-	if (P.y < m)
-	{
-		d1 = P.y;
-		phi1 = 90.0;
-		px_dash = m - P.y;
-		py_dash = P.z - n;
-
-		sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		phi1 = -90.0;
-		px_dash = P.y + m;
-
-		sol = checkLimits(phi1, backward_calc(phi1, px_dash, py_dash));
-		if (sol != nullptr)
-			ans.push_back(sol);
-
-		return &ans;
-
-	}
-	return nullptr;
+	return ans;
 }
 
 
 
 
-std::array<double, 5>* IVKinPos::forward_calc(double _phi1, double _px_dash, double _py_dash)
+bool IVKinPos::d1Condition(double _d1)
 {
-	double d3 = sqrt(_px_dash* _px_dash + _py_dash * _py_dash);
+	return (_d1 >= m || _d1 == 0);
+}
+
+std::array<double, 4>* IVKinPos::forward_calc(double _d1, Position _wristPoint)
+{
+	std::cout << "forward_calc: " << endl;
+	double px_dash, py_dash;
+	if (d1Condition(_d1))
+		px_dash = _d1 - m;
+	else
+		px_dash = m - _d1;
+
+	py_dash = _wristPoint.z - n;
+
 	double d2 = sqrt(o*o + b * b);
+	double d3 = sqrt(px_dash* px_dash + py_dash * py_dash);
+
 	double beta = rad2Deg(acos(((d3*d3) - (a*a) - (d2*d2)) / (-2 * a*d2)));
 	double alpha1 = rad2Deg(asin(sin(deg2Rad(beta)) * (d2 / d3)));
-	double alpha2 = rad2Deg(asin(_py_dash / d3));
-	double phi2_forward_upward = -1.0 * (alpha2 + alpha1);
-	double phi2_forward_downward = -1.0 * (alpha2 - alpha1);
+	double alpha2 = rad2Deg(asin(py_dash / d3));
+
+
+	
+
+	double phi2_forward_downward, phi2_forward_upward;
+	//if (d1Condition(_d1))
+	//{
+		phi2_forward_upward = -1.0 * (alpha2 + alpha1);
+		phi2_forward_downward = -1.0 * (alpha2 - alpha1);
+	/*}
+	else
+	{
+		phi2_forward_upward = 180.0-1.0 * (alpha2 + alpha1);
+		phi2_forward_downward = 180.0-1.0 * (alpha2 - alpha1);
+	}*/
+
 	double phi3_forward_upward = 360.0 - beta - rad2Deg(asin(b / d2)) - 90.0;
-	double phi3_forward_downward = -	1.0 *(90.0 - (360.0 - rad2Deg(asin(b / d2)) - (2 * 180.0 - beta)));
-	double phi3_forward_upward_1 = -1.0 * (90.0 + rad2Deg(asin(b / d2))- beta);
-	static std::array<double, 5> ans = 
+	double phi3_forward_downward = beta - rad2Deg(asin(b / d2)) - 90;
+
+	static std::array<double, 4> ans = 
 	{
 		phi2_forward_upward,
 		phi2_forward_downward,
 		phi3_forward_upward,
-		phi3_forward_downward,
-		phi3_forward_upward_1
+		phi3_forward_downward
 	};
+	std::cout << "phi2_forward_upward " << phi2_forward_upward << endl;
+	std::cout << "phi2_forward_downward " << phi2_forward_downward << endl;
+	std::cout << "phi3_forward_upward " << phi3_forward_upward << endl;
+	std::cout << "phi3_forward_downward " << phi3_forward_downward << endl;
 	return &ans;
 }
 
-std::array<double, 5>* IVKinPos::backward_calc(double _phi1, double _px_dash, double _py_dash)
+std::array<double, 4>* IVKinPos::backward_calc(double _d1, Position _wristPoint)
 {
-	double d3 = sqrt(_px_dash* _px_dash + _py_dash * _py_dash);
+	std::cout << "backward_calc: " << endl;
+
+	double px_dash = _d1 + m;
+	double py_dash = _wristPoint.z - n;
+
+	double d3 = sqrt(px_dash* px_dash + py_dash * py_dash);
 	double d2 = sqrt(o*o + b * b);
 	double beta = rad2Deg(acos(((d3*d3) - (a*a) - (d2*d2)) / (-2 * a*d2)));
 	double alpha1 = rad2Deg(asin(sin(deg2Rad(beta)) * (d2 / d3)));
-	double alpha2 = rad2Deg((asin(_py_dash / d3)));
-	double phi2_backward_upward = (alpha2 + alpha1) - 180.0; 
-	double phi2_backward_downward = (alpha2 - alpha1) - 180.0;
+	double alpha2 = rad2Deg((asin(py_dash / d3)));
 	
+	double phi2_backward_upward, phi2_backward_downward;
+	//if (d1Condition(_d1))
+	//{
+		phi2_backward_upward = (alpha2 + alpha1) - 180.0;
+		phi2_backward_downward = (alpha2 - alpha1) - 180.0;
+	//}
+	/*else
+	{
+		phi2_backward_upward = -(alpha2 - alpha1);
+		phi2_backward_downward = -(alpha2 - alpha1);
+	}*/
+
+	
+
 	double phi3_backward_upward = -1.0 * (90.0 - 1.0 * (beta - rad2Deg(asin(b / d2))));
-	double phi3_backward_downward = (360.0 - beta - rad2Deg(asin(b / d2)) - 90.0);
-	double phi3_backward_downward_1 = ((3 / 2) *180.0) - beta - rad2Deg(asin(b / d2));
-	static std::array<double, 5> ans =
+	double phi3_backward_downward = 360.0 - beta - rad2Deg(asin(b / d2)) - 90.0;
+	double phi3_backward_downward_1 = 270.0 - beta - rad2Deg(asin(b / d2));
+	static std::array<double, 4> ans =
 	{
 		phi2_backward_upward,
 		phi2_backward_downward,
 		phi3_backward_upward,
 		phi3_backward_downward,
-		phi3_backward_downward_1
 	};
+	std::cout << "phi2_backward_upward " << phi2_backward_upward << endl;
+	std::cout << "phi2_backward_downward " << phi2_backward_downward << endl;
+	std::cout << "phi3_backward_upward " << phi3_backward_upward << endl;
+	std::cout << "phi3_backward_downward " << phi3_backward_downward << endl;
 	return &ans;
 }
 
--- ctrl/src/ctrl/kinematics/inverse/IVKinPos.h
+++ ctrl/src/ctrl/kinematics/inverse/IVKinPos.h
@@ -3,6 +3,9 @@
 #include<math.h>
 #include <array>
 #include<vector>
+#include <SixDPos.h>
+#include <TMatrix.h>
+#include <Configuration.h>
 class IVKinPos
 {
 	/*
@@ -11,43 +14,41 @@ class IVKinPos
 
 public:
 	IVKinPos();
-	IVKinPos(double _x, double _y, double _z);
-
-	std::vector<std::array<double, 3>*>* get_IVKinPos();
-
 	~IVKinPos();
+
+	std::vector<std::array<double, 3>*>* get_IVKinPos(SixDPos* _pos);
+
+	struct Position
+	{
+		double x, y, z;
+		Position() : x(0), y(0), z(0) {};
+		Position(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+		inline Position operator+(Position a) {
+			return { a.x + x,a.y + y, a.z + z };
+		}
+	};
 private:
 	static constexpr double m = 0.330;
 	static constexpr double n = 0.645;
 	static constexpr double a = 1.150;
 	static constexpr double o = 0.115;
 	static constexpr double b = 1.220;
+	static constexpr double d_6 = 0.215;
 
+	static constexpr double marginSingularity = 0.001;
 
-		
-	struct Position
-	{
-		double x, y, z;
-		Position() : x(0), y(0), z(0) {};
-		Position(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
-	};
+	
 	double deg2Rad(double _deg);
 	double rad2Deg(double _rad);
-	Position P = Position(); // wristPointCenter
-	//double joints[3];
-	std::array<double, 3>* checkLimits(double _phi1, std::array<double, 5>* _solution);
-	std::vector<std::array<double, 3>*>* standardCase(double _phi1, Position _P);
-	std::array<double, 3>* otherCase1(double _phi1, double _d1, Position _P);
-	std::array<double, 3>* otherCase2(double _phi1, double _d1, Position _P);
-	//lässt sich mit weniger code implementieren
-	std::vector<std::array<double, 3>*>* checkphi1_case1(double _phi1, Position _P);
-	std::vector<std::array<double, 3>*>* checkphi1_case2(double _phi1, Position _P);
+	void checkLimits(double _phi1, std::array<double, 4>* _solution, std::vector<std::array<double, 3>*>* _ans);
+
+	std::vector<std::array<double, 3>*>* standardCase_phi1(double _phi1, Position _P);
 	
-	std::vector<std::array<double, 3>*>* specialcase1(Position _P);
-	std::vector<std::array<double, 3>*>* specialcase2(Position _P);
 
 
-	std::array<double,5>* forward_calc(double _phi1, double _px_dash, double _py_dash);
-	std::array<double,5>* backward_calc(double _phi1, double _px_dash, double _py_dash);
+	bool d1Condition(double _d1);
+	std::array<double,4>* forward_calc(double _d1, Position _wristPoint);
+	std::array<double,4>* backward_calc(double _d1, Position _wristPoint);
 };
 
