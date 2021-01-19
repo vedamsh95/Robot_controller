e35c80433561aca33ee33333ff19f8b0cad871fe
Sreenath Raghunathan <sreenath.raghunathan@st.ovgu.de>
2021-01-19 20:10:17 +0000

    inverse_kinematics.h edited online with Bitbucket

--- ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.h
+++ ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.h
@@ -1,17 +1,30 @@
 #ifndef SDRI_CTRL2019_INVERSE_KINEMATICS_H
 #define SDRI_CTRL2019_INVERSE_KINEMATICS_H
 
-#include<inverse_kinematics.cpp>
+
 #include <SixDPos.h>
-#include <TMatrix.h>
 #include <Configuration.h>
+#include<vector>
+
 
 /** \brief computes the inverse kinematics
  * This class is intended to handle the computation of the inverse kinematics. This includes both the computation of the
  * configurations for the position and the orientation.
  *
  */
+
 class InvKinematics {
+private:
+
+//define the Robot constants
+    const double d = 215;
+    const double m = 330;
+    const double n = 645;
+    const double o = 115;
+    const double a = 1150;
+    const double b = 1220;
+
+
 public:
     /**
      * This method computes all available configurations for a given position and returns them as a vector. It is the
@@ -21,10 +34,15 @@ public:
      * @param _pos {@ref SixDPos} to compute the configuration for
      * @return  a reference to a <code>vector<code> containing all possible configurations for the passed position
      */
-    vector<Configuration*>* get_inv_kinematics(SixDPos* _pos);
-    vector<double> findphi1(double Xc, double Yc, vector<double>& point);
-    void findphi2phi3(double Xc, double Yc, double Zc, double phi1, vector<double>& point2, vector<double>& point3);
-    void findphi3phi4phi5(double phi1, double phi2, double phi3, vector<double>& phi4, vector<double>& phi5, vector<double>& phi6, double* ptr);
+
+    std::vector<Configuration*>* get_inv_kinematics(SixDPos* _pos);
+    void findphi1(double Xc, double Yc, std::vector<double>& point);
+    void findphi2phi3(double Xc, double Yc, double Zc, double phi1, std::vector<double>& point2, std::vector<double>& point3);
+    void findphi3phi4phi5(double phi1, double phi2, double phi3, std::vector<double>& phi4, std::vector<double>& phi5, std::vector<double>& phi6, double* ptr2);
+    std::tuple<double, double> ForwardsElbowdown(double d1, double Zc);
+    std::tuple<double, double> ForwardsElbowup(double d1, double Zc);
+    std::tuple<double, double> BackwardsElbowdown(double d1, double Zc);
+    std::tuple<double, double> BackwardsElbowup(double d1, double Zc);
 
 };
 #endif //SDRI_CTRL2019_INVERSE_KINEMATICS_H
