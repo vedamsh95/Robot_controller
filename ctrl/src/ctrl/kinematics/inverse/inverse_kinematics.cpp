ec50896d36b646bbadafd3adf7edc7e8c00dab40
mukesh ghosh <mukeshghosh.1993@hotmail.com>
2021-01-19 18:10:10 +0100

    Inverse kinematics position and orientation part full

--- ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
+++ ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
@@ -16,6 +16,9 @@ using namespace std;
 
 std::vector<double> find_theta1(double X_c, double Y_c);
 void find_theta2_theta3(double X_c, double Y_c, double Z_c, vector<double>& arr2, vector<double>& arr3);
+std::vector<vector<double>> find_rotational_matrix_R0_3(double theta_1, double theta_2, double theta_3);
+void find_theta4_theta5_theta6(double a_x, double a_y, double a_z, double n_z, double s_z, vector<double>& arr4, vector<double>& arr5, vector<double>& arr6);
+
 
 vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 {
@@ -36,7 +39,7 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 
 
     // Les's find matrix R0_6 by using Raw Yaw and Pitch angle 
-    double R0_6[3][3];
+    //double R0_6[3][3];
     double r11, r12, r13, r21, r22, r23, r31, r32, r33;
 
     r11 = cos(new_roll) * cos(new_pitch);
@@ -49,15 +52,12 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
     r32 = cos(new_pitch) * sin(new_yaw);
     r33 = cos(new_pitch) * cos(new_yaw);
 
-    R0_6[0][0] = r11;
-    R0_6[0][1] = r12;
-    R0_6[0][2] = r13;
-    R0_6[1][0] = r21;
-    R0_6[1][1] = r22;
-    R0_6[1][2] = r23;
-    R0_6[2][0] = r31;
-    R0_6[2][1] = r32;
-    R0_6[2][2] = r33;
+    std::vector<vector<double>> R0_6
+    {
+        {r11,r12,r13},
+        {r21,r22,r23},
+        {r31,r32,r33}
+    };
 
     //let's compute the wrist center position Oc [Xc, Yc, Zc]
     double X_c, Y_c, Z_c;
@@ -67,7 +67,7 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 
    
     //Compute the theta1 here
-    std::vector<double> theta1_arr, theta2_arr, theta3_arr;
+    std::vector<double> theta1_arr, theta2_arr, theta3_arr, theta4_arr, theta5_arr, theta6_arr;
     theta1_arr = find_theta1(X_c, Y_c);
 
     //vector<double>theta1_arr;
@@ -75,6 +75,17 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 
     //Compute the theta2 and theta3 here
 
+    std::vector<vector<double>> inv_R0_3, R3_6;
+    R3_6 =
+    {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+    };
+    double a_x, a_y, a_z, n_z, s_z;
+    double th_1, th_2, th_3;
+
+    /*
     for (auto elem1 : theta1_arr)
     {
         if (-185 < elem1 < 185)
@@ -95,7 +106,68 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 
  
     }
+    */
 
+    std::vector<int>::size_type sz = theta1_arr.size();
+
+    for (unsigned j = 0; j < sz; j++)
+    {
+        th_1 = theta1_arr[j];
+
+        if (-185 < theta1_arr[j] < 185)
+        {
+            find_theta2_theta3(X_c, Y_c, Z_c, theta2_arr, theta3_arr);
+
+            std::vector<int>::size_type az = theta2_arr.size();
+            for (unsigned i = 0; i < az; i++)
+            {
+
+
+                th_2 = theta2_arr[i];
+                th_3 = theta3_arr[i];
+                cout << "Theta 1: " << th_1 << " Theta 2: " << th_2 << " Theta 3: " << th_3 << endl;
+
+
+                // Here we are finding base rotational matrix R0_3
+                inv_R0_3 = find_rotational_matrix_R0_3(th_1, th_2, th_3);
+
+
+
+                // Here we are multiplying first two rotational matrix R0_3 and R0_6
+                for (int l = 0; l < 3; l++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+
+                        for (int k = 0; k < 3; k++)
+                        {
+                            R3_6[l][j] += inv_R0_3[l][k] * R0_6[k][j];
+                        }
+                    }
+                }
+
+
+                //Here are finding the angles theta 4, theta 5, theta 6
+                a_x = R3_6[0][2];
+                a_y = R3_6[1][2];
+                a_z = R3_6[2][2];
+                n_z = R3_6[2][0];
+                s_z = R3_6[2][1];
+                find_theta4_theta5_theta6(a_x, a_y, a_z, n_z, s_z, theta4_arr, theta5_arr, theta6_arr);
+
+                if (-140 < theta2_arr[i] < -5 && -120 < theta3_arr[i] < 168)
+                {
+                    solutions->push_back(new Configuration({ theta1_arr[j],theta2_arr[i],theta3_arr[i],theta4_arr[i],theta5_arr[i],theta6_arr[i] }));
+                }
+                else
+                    break;
+            }
+        }
+        else if (theta1_arr[j] < -185 || theta1_arr[j] >185)
+            break;
+
+
+    }
 
 
 
@@ -206,7 +278,7 @@ void find_theta2_theta3(double X_c, double Y_c, double Z_c, vector<double>& arr2
         Py_dash = Z_c - n;
         d_3 = sqrt(Px_dash * Px_dash + Py_dash * Py_dash);
         beta1 = acos(((d_3 * d_3) - (a * a) - (d_2 * d_2)) / (-2 * a * d_2)) * 180 / PI;
-        alpha1 = asin(sin(beta1) * (d_2 / d_3)) * 180 / PI;
+        alpha1 = asin(sin(beta1 * PI / 180) * (d_2 / d_3)) * 180 / PI;
         alpha2 = asin(Py_dash / d_3) * 180 / PI;
         //forward elbow down
 
@@ -229,7 +301,7 @@ void find_theta2_theta3(double X_c, double Y_c, double Z_c, vector<double>& arr2
         Py_dash = Z_c - n;
         d_3 = sqrt(Px_dash * Px_dash + Py_dash * Py_dash);
         beta1 = acos(((d_3 * d_3) - (a * a) - (d_2 * d_2)) / (-2 * a * d_2)) * 180 / PI;
-        alpha1 = asin(sin(beta1) * (d_2 / d_3)) * 180 / PI;
+        alpha1 = asin(sin(beta1 * PI / 180) * (d_2 / d_3)) * 180 / PI;
         alpha2 = asin(Py_dash / d_3) * 180 / PI;
         //bacward elbow down
 
@@ -247,3 +319,269 @@ void find_theta2_theta3(double X_c, double Y_c, double Z_c, vector<double>& arr2
         arr3.push_back(theta3);
     }
 }
+
+std::vector<vector<double>> find_rotational_matrix_R0_3(double theta_1, double theta_2, double theta_3)
+{
+    double nx, ny, nz, sx, sy, sz, ax, ay, az, th, al;
+    double alpha[4] = { 180, 90, 0, 90 };
+    double theta[4] = { 0, theta_1, theta_2, theta_3 - 90 };
+    std::vector<vector<double>> R0_1, R1_2, R2_3, R3_4, R0_2, R2_4, R0_3, new_R0_3;
+
+    R0_2 =
+    {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+    };
+
+    R2_4 =
+    {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+    };
+
+    R0_3 =
+    {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+    };
+
+    new_R0_3 =
+    {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+    };
+
+    //Here we are finding the rotation matrix R0_1
+    {
+        th = theta[0];
+        al = alpha[0];
+        nx = cos(th * PI / 180);
+        sx = -sin(th * PI / 180) * cos(al * PI / 180);
+        ax = sin(th * PI / 180) * cos(al * PI / 180);
+        ny = sin(th * PI / 180);
+        sy = cos(th * PI / 180) * cos(al * PI / 180);
+        ay = -cos(th * PI / 180) * sin(al * PI / 180);
+        nz = 0;
+        sz = sin(al * PI / 180);
+        az = cos(al * PI / 180);
+        R0_1 =
+        {
+            {nx,sx,ax},
+            {ny,sy,ay},
+            {nz,sy,ay}
+        };
+    }
+
+    //Here we are finding the rotation matrix R1_2
+    {
+        th = theta[1];
+        al = alpha[1];
+        nx = cos(th * PI / 180);
+        sx = -sin(th * PI / 180) * cos(al * PI / 180);
+        ax = sin(th * PI / 180) * cos(al * PI / 180);
+        ny = sin(th * PI / 180);
+        sy = cos(th * PI / 180) * cos(al * PI / 180);
+        ay = -cos(th * PI / 180) * sin(al * PI / 180);
+        nz = 0;
+        sz = sin(al * PI / 180);
+        az = cos(al * PI / 180);
+        R1_2 =
+        {
+            {nx,sx,ax},
+            {ny,sy,ay},
+            {nz,sy,ay}
+        };
+    }
+
+    //Here we are finding the rotation matrix R2_3
+    {
+        th = theta[2];
+        al = alpha[2];
+        nx = cos(th * PI / 180);
+        sx = -sin(th * PI / 180) * cos(al * PI / 180);
+        ax = sin(th * PI / 180) * cos(al * PI / 180);
+        ny = sin(th * PI / 180);
+        sy = cos(th * PI / 180) * cos(al * PI / 180);
+        ay = -cos(th * PI / 180) * sin(al * PI / 180);
+        nz = 0;
+        sz = sin(al * PI / 180);
+        az = cos(al * PI / 180);;
+        R2_3 =
+        {
+            {nx,sx,ax},
+            {ny,sy,ay},
+            {nz,sy,ay}
+        };
+    }
+
+    //Here we are finding the rotation matrix R3_4
+    {
+        th = theta[3];
+        al = alpha[3];
+        nx = cos(th * PI / 180);
+        sx = -sin(th * PI / 180) * cos(al * PI / 180);
+        ax = sin(th * PI / 180) * cos(al * PI / 180);
+        ny = sin(th * PI / 180);
+        sy = cos(th * PI / 180) * cos(al * PI / 180);
+        ay = -cos(th * PI / 180) * sin(al * PI / 180);
+        nz = 0;
+        sz = sin(al * PI / 180);
+        az = cos(al * PI / 180);
+        R3_4 =
+        {
+            {nx,sx,ax},
+            {ny,sy,ay},
+            {nz,sy,ay}
+        };
+    }
+
+    // Here we are multiplying first two rotational matrix R0_1 and R1_2
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+
+            for (int k = 0; k < 3; k++)
+            {
+                R0_2[i][j] += R0_1[i][k] * R1_2[k][j];
+            }
+        }
+    }
+
+    // Here we are multiplying last two rotational matrix R2_3 and R3_4
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+
+            for (int k = 0; k < 3; k++)
+            {
+                R2_4[i][j] += R2_3[i][k] * R3_4[k][j];
+            }
+        }
+    }
+
+    // Here we are multiplying the result of multiplication and we will get the R0_3
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+
+            for (int k = 0; k < 3; k++)
+            {
+                R0_3[i][j] += R0_2[i][k] * R2_4[k][j];
+            }
+        }
+    }
+
+    // finidng the inverse matrix of R0_3
+    double determinant = 0;
+
+    //here we are solving the determinant of the rotational matrix
+    for (int i = 0; i < 3; i++)
+    {
+        determinant += R0_3[0][i] * ((R0_3[1][(i + 1) % 3] * R0_3[2][(i + 2) % 3]) - (R0_3[1][(i + 2) % 3] * R0_3[2][(i + 1) % 3]));
+    }
+
+    //Here we are going to find the inverse matrix using basic mathmatics formula
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            new_R0_3[i][j] = ((R0_3[(j + 1) % 3][(i + 1) % 3] * R0_3[(j + 2) % 3][(i + 2) % 3]) - (R0_3[(j + 1) % 3][(i + 2) % 3] * R0_3[(j + 2) % 3][(i + 1) % 3])) / determinant;
+        }
+    }
+
+
+    return new_R0_3;
+
+}
+
+void find_theta4_theta5_theta6(double a_x, double a_y, double a_z, double n_z, double s_z, vector<double>& arr4, vector<double>& arr5, vector<double>& arr6)
+{
+    double theta_4, theta_5, theta_6;
+
+    //here we are conmputing theta4
+    theta_4 = atan2(-a_y, -a_x) * 180 / PI;
+    if ((theta_4 - 360) > -350)
+    {
+        arr4.push_back(theta_4 - 360);
+        arr4.push_back(theta_4);
+    }
+    else if ((theta_4 + 360) < 350)
+    {
+        arr4.push_back(theta_4 + 360);
+        arr4.push_back(theta_4);
+    }
+
+    // here we are computing theta5 and theta6
+    theta_5 = atan2(sqrt(1 - (a_z * a_z)), -a_z) * 180 / PI;
+    theta_6 = atan2(s_z, n_z) * 180 / PI;
+    if ((theta_6 - 360) > -350)
+    {
+        arr5.push_back(theta_5);
+        arr5.push_back(theta_5);
+        arr6.push_back(theta_6 - 360);
+        arr6.push_back(theta_6);
+
+    }
+    else if ((theta_6 + 360) < 350)
+    {
+        arr5.push_back(theta_5);
+        arr5.push_back(theta_5);
+        arr6.push_back(theta_6 + 360);
+        arr6.push_back(theta_6);
+    }
+
+    theta_5 = -theta_5;
+    if (theta_4 > 0)
+    {
+        theta_4 = theta_4 - 180;
+    }
+    else
+    {
+        theta_4 = theta_4 + 180;
+    }
+
+    if ((theta_4 - 360) > -350)
+    {
+        arr4.push_back(theta_4 - 360);
+        arr4.push_back(theta_4);
+    }
+    else if ((theta_4 + 360) < 350)
+    {
+        arr4.push_back(theta_4 + 360);
+        arr4.push_back(theta_4);
+    }
+
+    if (theta_6 > 0)
+    {
+        theta_6 = theta_6 - 180;
+    }
+    else
+    {
+        theta_6 = theta_6 + 180;
+    }
+
+    if ((theta_6 - 360) > -350)
+    {
+        arr5.push_back(theta_5);
+        arr5.push_back(theta_5);
+        arr6.push_back(theta_6 - 360);
+        arr6.push_back(theta_6);
+
+    }
+    else if ((theta_6 + 360) < 350)
+    {
+        arr5.push_back(theta_5);
+        arr5.push_back(theta_5);
+        arr6.push_back(theta_6 + 360);
+        arr6.push_back(theta_6);
+    }
+
+}
+
