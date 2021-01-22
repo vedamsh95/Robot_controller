cc0ca465a3cca57edaeed22b94ea84a7f8fe6d41
Timo <timo.schmidt1@st.ovgu.de>
2021-01-22 22:34:17 +0100

    Introduction of ConfigProvider Class

---
+++ ctrl/src/ctrl/Config/ConfigProvider.h
@@ -0,0 +1,33 @@
+//
+// Created by Timo on 22.01.2021.
+//
+
+#ifndef SDIR_CTRL2020_CONFIGPROVIDER_H
+#define SDIR_CTRL2020_CONFIGPROVIDER_H
+
+class ConfigProvider{
+private:
+    ConfigProvider(){}
+    int paramA = 100; // examples
+    int paramB = 200; // example
+    int paramC = 300; // example
+
+    //ToDo: Insert paramters for boundaries, etc.
+
+public:
+    static ConfigProvider& getInstance(){
+        static ConfigProvider instance;
+        return instance;
+    }
+    ConfigProvider(ConfigProvider const&) = delete;
+    void operator=(ConfigProvider const&) = delete;
+
+    //ToDo: Insert Getter-Function for specific parameter --> Usage of ConfigProvider in specific class: ConfigProvider::getInstance().getParamA()
+    int getParamA() const{return this->paramA; }
+    int getParamB() const{return this->paramB; }
+    int getParamC() const{return this->paramC; }
+
+};
+
+#endif //SDIR_CTRL2020_CONFIGPROVIDER_H
+
