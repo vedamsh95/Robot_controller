3417d0b29157eef5bedeca00ee95d6c36d365363
Seb <sebastian.hamacher@st.ovgu.de>
2021-01-23 00:58:33 +0100

    implemented selecting configs

--- ctrl/src/ctrl/pathplanner/lin/lin.h
+++ ctrl/src/ctrl/pathplanner/lin/lin.h
@@ -29,7 +29,7 @@ public:
     std::array<double,3> trapezoidal_prof(Vector<double, 3> start_pos, Vector<double, 3> end_pos,
                                        double t_f, double t, double t_c, double a_max, double v_max, Vector<double, 3> path_dir_vec);
     std::array<double,3> max_vel_profile(Vector<double, 3> start_pos,double distance, double t_f, double t_m, double t, Vector<double, 3> path_dir_vec );
-
+    double calc_config_difference(Configuration* config1, Configuration* config2);
 
 
 };
