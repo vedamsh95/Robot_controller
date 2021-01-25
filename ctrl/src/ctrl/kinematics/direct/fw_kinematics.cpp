e7fe32bd3774ad7bf934de98e47bf56c2230b20d
Justus Wagner <justus.wagner@st.ovgu.de>
2021-01-25 12:34:36 +0000

    fw_kinematics.cpp edited online with Bitbucket

--- ctrl/src/ctrl/kinematics/direct/fw_kinematics.cpp
+++ ctrl/src/ctrl/kinematics/direct/fw_kinematics.cpp
@@ -7,90 +7,119 @@ using namespace std;
 
 SixDPos* FwKinematics::get_fw_kinematics(Configuration* _cfg)
 {
+    for (int i = 0; i < 6; i++) {
+        cout << i << "=i,    " << _cfg->get_configuration()[i] << "  ";
+        cout << endl;
+    }
+
+
     
     double* M0 = new double[16];
-    M0 = D_H_matrix(0, 3.14, 0, 645);
-    for (int i = 0; i < 16; i++) {
-        cout << M0[i] << " ";
-    }
-    cout << endl;
+    M0 = D_H_matrix(0, M_PI, 0, 0.645);
+
+ 
+
     
     double phi1 = _cfg->get_configuration()[0];
 
     double* M1 = new double[16];
-    M1 = D_H_matrix(phi1, 1.57, 330, 0);
-    for (int i = 0; i < 16; i++) {
-        cout << M1[i] << " ";
-    }
-    cout << endl;
+    M1 = D_H_matrix(phi1, M_PI*0.5, 0.330, 0);
+
 
     double* M0x1 = new double[16];
     M0x1 = matrix_mult(M0, M1);
+
+    //delete M0;
+    //delete M1;
     
     double phi2 = _cfg->get_configuration()[1];
 
     double* M2 = new double[16];
-    M2 = D_H_matrix(phi2, 0, 1150, 0);
-    for (int i = 0; i < 16; i++) {
-        cout << M2[i] << " ";
-    }
-    cout << endl;
+    M2 = D_H_matrix(phi2, 0, 1.150, 0);
+
 
     double* M0x1x2 = new double[16];
     M0x1x2 = matrix_mult(M0x1, M2);
 
+    //delete M0x1;
+    //delete M2;
+
     double phi3 = _cfg->get_configuration()[2];
 
     double* M3 = new double[16];
-    M3 = D_H_matrix((phi3 - 1.57), 1.57, 115, 0);
+    M3 = D_H_matrix((phi3 - M_PI*0.5), M_PI*0.5, 0.115, 0);
+
+    
+
+    cout << "M3:" << endl;
     for (int i = 0; i < 16; i++) {
-        cout << M3[i] << " ";
+        cout << i << "=i,    " << M3[i] << "  ";
+        cout << endl;
     }
+    
 
     double* M0x1x2x3 = new double[16];
     M0x1x2x3 = matrix_mult(M0x1x2, M3);
 
+
+
+    
+
+
     double phi4 = _cfg->get_configuration()[3];
 
     double* M4 = new double[16];
-    M4 = D_H_matrix(phi4, -1.57, 0, -1220);
-    for (int i = 0; i < 16; i++) {
-        cout << M4[i] << " ";
-    }
+    M4 = D_H_matrix(phi4, M_PI*-0.5, 0, -1.220);
+
+
+    
 
     double* M0x1x2x3x4 = new double[16];
     M0x1x2x3x4 = matrix_mult(M0x1x2x3, M4);
 
+
+
     double phi5 = _cfg->get_configuration()[4];
 
     double* M5 = new double[16];
-    M5 = D_H_matrix(phi5, 1.57, 0, 0);
-    for (int i = 0; i < 16; i++) {
-        cout << M5[i] << " ";
-    }
+    M5 = D_H_matrix(phi5, M_PI*0.5, 0, 0);
+
+
+    
 
     double* M0x1x2x3x4x5 = new double[16];
     M0x1x2x3x4x5 = matrix_mult(M0x1x2x3x4, M5);
 
+
+
     double phi6 = _cfg->get_configuration()[5];
 
     double* M6 = new double[16];
-    M6 = D_H_matrix((3.1416 + phi6), 3.1416, 0, -215);
-    for (int i = 0; i < 16; i++) {
-        cout << M6[i] << " ";
-    }
+
+    M6 = D_H_matrix((M_PI + phi6), M_PI, 0, -0.215);
+
+
+   
 
     double* M_fin = new double[16];
     M_fin = matrix_mult(M0x1x2x3x4x5, M6);
- 
+
+    
+
+
+    cout << "M_fin:" << endl;
+    for (int i = 0; i < 16; i++) {
+        cout << i << "=i,    " << M_fin[i] << "  ";
+        cout << endl;
+    }
+    cout << "atan2(M_fin[4], M_fin[0])= " << atan2(M_fin[4], M_fin[0]) << "  " << atan2(M_fin[4], M_fin[0]) * 180 / M_PI;
 
     if (M_fin[0]== 0 && M_fin[4] == 0) {
-        cout << M_fin[0] << " " << M_fin[1] << " " << M_fin[2] << " " << M_fin[3] << " " << M_fin[4] << " " << M_fin[5] << " " << M_fin[6];
-        return new SixDPos(M_fin[3], M_fin[7], M_fin[11], asin(-M_fin[1]), -M_fin[8] * 1.57, 0);
+        
+        return new SixDPos(M_fin[3], M_fin[7], M_fin[11], asin(-M_fin[1]), -M_fin[8] * M_PI*0.5, 0);
     }
     else {
-        
-    
+        cout << "atan2(M_fin[4], M_fin[0])= " << atan2(M_fin[4], M_fin[0]) << "  " << atan2(M_fin[4], M_fin[0]) *180/M_PI;
         return new SixDPos(M_fin[3], M_fin[7], M_fin[11], atan2(M_fin[4], M_fin[0]), atan2(-M_fin[8], sqrt(M_fin[9] * M_fin[9] + M_fin[10] * M_fin[10])), atan2(M_fin[9], M_fin[10]));
     }
 
@@ -106,7 +135,7 @@ SixDPos* FwKinematics::get_fw_kinematics(Configuration* _cfg)
 double* FwKinematics::D_H_matrix(double angle, double a, double r, double d) {
 
     double* p = new double[16];
-   
+
     p[0] = cos(angle);
 
     p[1] = -1 * sin(angle) * cos(a);
@@ -118,8 +147,8 @@ double* FwKinematics::D_H_matrix(double angle, double a, double r, double d) {
     p[4] = sin(angle);
 
     p[5] = cos(angle) * cos(a);
- 
-    p[6] = -1 *cos(angle) * sin(a);
+
+    p[6] = -1 * cos(angle) * sin(a);
 
     p[7] = r * sin(angle);
 
@@ -148,22 +177,26 @@ double* FwKinematics::matrix_mult(double* mat_a, double* mat_b) {
 
   
     double* mat = new double[16];
-   
+
 
     double sum = 0;
 
     for (int i=0; i < 4; i++) {
         for (int k=0; k < 4; k++) {
             for (int l = 0; l < 4; l++) {
+            
                 sum = sum + mat_a[4 * i + l] * mat_b[4 * l + k];
+               
             }
             mat[4 * i + k] = sum;
             sum = 0;
         }
     }
+  
+ 
 
     return mat;
-
+    
 
 }
 
