daa4a3d7cd3293ef612338cc91f35f5e292138da
Sreenath Raghunathan <sreenath.raghunathan@st.ovgu.de>
2021-01-29 11:09:54 +0000

    singularity conditions added & code cleanup

--- ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
+++ ctrl/src/ctrl/kinematics/inverse/inverse_kinematics.cpp
@@ -1,5 +1,7 @@
 #include "inverse_kinematics.h"
 #include "inverse_kinematics.h"
+#include "inverse_kinematics.h"
+#include "inverse_kinematics.h"
 #define _USE_MATH_DEFINES
 #include "inverse_kinematics.h"
 #include <math.h>
@@ -9,25 +11,19 @@
 #include <iostream>
 
 
-//sample results for input values of (x=0.8, y = -0.3, z = 2 , e1 = 0, e2 = 90, e3 = 0
+//steps
+//1.  get final TCP details from the pointer sent to get_inv_kinematics function
+//2.  take the overall transformation matrix from TMatrix function
+//3.  find WCP
+//4.  find phi1 call phi1 function, return an array of possible phi1 values
+//5.  take each values of phi1, check if it meets the criteria, call function to find phi2 and phi3
+//6.  if the returned array of phi2 and phi3 within possible values w.r.t the robot configurations, proceed to find phi4,phi5 and phi6
+//7.  call phi4,phi5,phi6 function and return array of possible values
+//8.  push to the possible set of configuration 
+//9.  function to find phi1
+//10. function to find phi2 and phi3
+//11. function to find phi4, phi5 and phi6
 
-//sample values obtained
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 59.0788  phi5 : 94.2651  phi6 : 296.434
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -120.921  phi5 : -94.2651  phi6 : -243.566
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -120.921  phi5 : -94.2651  phi6 : -243.566
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -120.921  phi5 : -94.2651  phi6 : 116.434
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 239.079  phi5 : -94.2651  phi6 : -243.566
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 239.079  phi5 : -94.2651  phi6 : -243.566
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 239.079  phi5 : -94.2651  phi6 : 116.434
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 3.32495  phi5 : 113.56phi6 : -61.7082
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 3.32495  phi5 : 113.56phi6 : -61.7082
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 3.32495  phi5 : 113.56phi6 : 298.292
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -176.675  phi5 : -113.56  phi6 : -241.708
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -176.675  phi5 : -113.56  phi6 : -241.708
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : -176.675  phi5 : -113.56  phi6 : 118.292
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 183.325  phi5 : -113.56phi6 : -241.708
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 183.325  phi5 : -113.56phi6 : -241.708
-//phi1 : -153.729  phi2 : -82.542  phi3 : -78.5476  phi4 : 183.325  phi5 : -113.56phi6 : 118.292
 
 
 vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
@@ -41,7 +37,7 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
     double e2 = _pos->get_B();
     double e3 = _pos->get_C();
 
-    //test 1 (x =1.75, y =2, z = 1.91, e1 =0.0, e2 = 90, e3 = 0) in GUI
+    //cout << "\ntest 1 \n ";
     //cout<< "Printing Xp, Yp, Zp, e1, e2 and e3" << "\n";
     //cout<< Xp << "  " << Yp << "  " << Zp << "  " << e1 << "  " << e2 << "  " << e3<<"\n " ;  
     //cout<< "\n";
@@ -51,14 +47,16 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
     e2 = (e2 * 180) / M_PI;
     e3 = (e3 * 180) / M_PI;
 
+    //cout << "\ntest 2 \n ";
     //cout << "\n Printing angles in main program\n";
-    //cout << e1 <<"  "<< e2 << "   " << e3 << "\n";
+    //cout <<"e1 :"<< e1 <<"  "<<"e2 :"<< e2 << "   " <<"e3 :"<< e3 << "\n";
 
 //transformation matrix (in th form of an array)
     double tansformationmatrix[16];
     TMatrix Trans(e1, e2, e3, Xp, Yp, Zp);
     Trans.getTmatrix(tansformationmatrix[0]);
 
+    //cout << "\ntest 3 \n ";
     //cout << "\ntransformation matrix\n";
     //for (int f = 0; f < 16; f++)
     //    cout << tansformationmatrix[f] << "  ";
@@ -70,59 +68,53 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
     double Zc = Zp - (d * tansformationmatrix[10]);
 
 
-    ////test 3
+    ////test 4
+    //cout << "\ntest 4 \n ";
     //cout << "\nPrint wrist center point\n" << Xc << "  " << Yc << "  " << Zc << "\n";
-  
-//find possible values of phi1
-    vector<double>temparrayphi1;
-    findphi1(Xc, Yc, temparrayphi1);
+
+    if (Xp > 2.701 || Xp < -2.701 || Zp > 3.020 || Zp < -0.481)
+    {
+        //print out of scope in GUI
+    }
 
 
-    //// //test 4
-    //cout<< "\nprint tempphi1array\n";
-    //for(int f = 0; f < temparrayphi1.size(); f++)
-    //              {
-    //                 cout << temparrayphi1[f] << "\n";
-    //               }
+//finding first possible phi1 value
+//shoulder singularity
+    double tempphi1;
+    if (Xc == 0.000 && Yc == 0.000)
+            {
+                    if (Xp == 0.000 && Yp == 0.000)  
+                    { 
+                        //to be dealt in the LIN / SPLINE to avoid this point and take average of the previous and next points
+                        tempphi1 = 0.000; 
+                    }
+                    else 
+                    { 
+                        tempphi1 = -(atan2(Yp, Xp) * (180 / M_PI));
+                    }
+            }
+    else
+            {  
+                tempphi1 = -(atan2(Yc, Xc) * (180 / M_PI)); 
+            }
 
 
     vector<double> arrayphi1, arrayphi2, arrayphi3;
     vector<double> phi4, phi5, phi6;
 
 
-
-//find possible conbinations of phi1, phi2, phi3
-
-    for (int i = 0; i < temparrayphi1.size(); i++)
-    {
-        double phi1 = temparrayphi1[i];
-
-        findphi2phi3(Xc, Yc, Zc, phi1, arrayphi1, arrayphi2, arrayphi3);
-    }
+//finding all combinations of possible phi1, phi2 and phi3
+   find_phi_123(Xc, Yc, Zc, tempphi1, arrayphi1, arrayphi2, arrayphi3);
 
 
-    ////test 
-    //for (int f = 0; f < arrayphi2.size(); f++)
-    //{
-    //    cout << "\nprinting elemnts in main program to check all combinations of phi1, phi2 and phi3\n";
-    //    cout << arrayphi1[f] << "  " << arrayphi2[f] << "  " << arrayphi3[f] << "\n";
-    //}
-
-    //cout << "checking array size : \n";
-    //cout << arrayphi1.size();
-    //cout << arrayphi2.size();
-    //cout << arrayphi3.size();
-    //cout << "\n";
-
-    
     for (int j = 0; j < arrayphi1.size(); j++)
     {
         if ((arrayphi2[j] > double(-140)) && (arrayphi2[j]< double(-5)) && (arrayphi3[j] > double(-120)) && (arrayphi3[j]<double(168)))
             {
-                //
-                //cout << arrayphi1[j] << "  " << arrayphi2[j] << "  " << arrayphi3[j] << "\n";
+
                 findphi4phi5phi6(arrayphi1[j], arrayphi2[j], arrayphi3[j], phi4, phi5, phi6, tansformationmatrix);
 
+                ////test 9
                 //cout << "testing for return values from function to satisfy the conditions\n";
                 //for (int e = 0; e < phi4.size(); e++)
                 //{
@@ -131,15 +123,17 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 
                 for (int r = 0; r < phi4.size(); r++)
                         {
+                                    ////test 10:
+                                    //cout << "\ntest 10 \n ";
                                     if ((phi4[r] > -350 && phi4[r] < 350) && (phi5[r] > -125 && phi5[r] < +125) && (phi6[r] > -350 && phi6[r] < 350))
                                     {
-                                        cout << " phi1: " << arrayphi1[j] << "  phi2 :" << arrayphi2[j] << "  phi3  : " << arrayphi3[j] << "  phi4:  " << phi4[r] << "  phi5:  " << phi5[r] << "  phi6:  " << phi6[r]<<"\n";
+
+                                        cout << "config no: "<< r <<" :"<< "phi1: " << arrayphi1[j] << "  phi2 :" << arrayphi2[j] << "  phi3  : " << arrayphi3[j] << "  phi4:  " << phi4[r] << "  phi5:  " << phi5[r] << "  phi6:  " << phi6[r]<<"\n";
                                         solutions->push_back(new Configuration({arrayphi1[j], arrayphi2[j],arrayphi3[j],phi4[r], phi5[r],phi6[r] }));
                                     }
                                     else
                                     {
                                     }
-
                         }
         }
         else
@@ -154,75 +148,36 @@ vector<Configuration*>* InvKinematics::get_inv_kinematics(SixDPos* _pos)
 }
 
 
-void InvKinematics::findphi1(double Xc, double Yc, std::vector<double>& point)
+void InvKinematics::find_phi_123(double Xc, double Yc, double Zc, double phi1, std::vector<double> (&point1), std::vector<double> (&point2), std::vector<double> (&point3))
 {
 
-//changed this function from last time because backward cases was giving duplicate values
-//now "findphi1" function only calculates possible forward cases of phi1, backward case is handled in the "findphi2phi3" function
-
-    double phi1 = atan2(Yc, Xc) *(180 / M_PI);   // in angles
-
-
-    if ((5 < phi1 && phi1 < 175) || ( -5 > phi1 && phi1 > -175))
-    {
-        if (Xc > 0 && Yc > 0) //first quadrant
-        {
-            point.push_back(-phi1);                //push the negative value of Phi1 into array because it is anticlockwise to +y direction
-            
-        }
-        else if (Xc > 0 && Yc < 0)
-        {
-            point.push_back(-phi1);
-
-        }
-        else if (Xc < 0 && Yc>0)
-        {
-            point.push_back(-(phi1+180));
-
-        }
-        else if (Xc < 0 && Yc < 0)
-        {
-            point.push_back(180-phi1);
-
-        }
-    }
-
-    //
-    //cout << "\nPrinting value of phi1 inside findphi1 function\n" << point[0]<<"\n";
-    
-}
-
-void InvKinematics::findphi2phi3(double Xc, double Yc, double Zc, double phi1, std::vector<double> (&point1), std::vector<double> (&point2), std::vector<double> (&point3))
-{
-
-    
     double d1 = sqrt((Xc * Xc) + (Yc * Yc));
 
-    //cout <<"d1 is "<< d1<<"\n";
-    //cout << "ph1 is"<< phi1 << "\n";
+    //Elbow singularity
+    double Px, Py, d3;
+    Px = d1 - m;
+    Py = Zc - n;
+    d3 = sqrt((Px * Px) + (Py * Py));
+
+    while (d3 > double(1150 + 1220))
+    {
+
+        d1 = (d1 *0.95);
+        Zc = (Zc *0.95);
+
+        Px = d1 - m;
+        Py = Zc - n;
+        d3 = sqrt((Px * Px) + (Py * Py));
+
+    }
 
     double FEd2, FEd3, FEup2, FEup3, BEd2, BEd3, BEup2, BEup3;
 
-    //find all values of possible phi2 and phi3 with the d1 and Z value
-    //
-
+    //4 possible cases
     std::tie(FEd2, FEd3) = ForwardsElbowdown(d1, Zc);
-    //cout << " Fed2  "<<FEd2 <<"\n"<< " Fed3 "<< FEd3<<"\n";
-    //
-
     std::tie(FEup2, FEup3) = ForwardsElbowup(d1, Zc);
-    //cout << " FEup2 "<< FEup2 << "\n" <<" FEup3 "<< FEup3 << "\n";
-    //
-
-
     std::tie(BEup2, BEup3) = BackwardsElbowup(d1, Zc);
-    //cout <<"BEup2"<< BEup2 << "\n" <<" BEup3"<< BEup3 << "\n";
-    //
-
-
     std::tie(BEd2, BEd3) = BackwardsElbowdown(d1, Zc);
-    //cout << "BEd2"<< BEd2 << "\n" << "BEd3"<< BEd3 << "\n";
-    //
 
 
         if ((phi1 > 5 && phi1 < 175) || (phi1<-5 && phi1>-175))
@@ -250,7 +205,6 @@ void InvKinematics::findphi2phi3(double Xc, double Yc, double Zc, double phi1, s
             }
 
         }
-
         else if (phi1 < 5 && phi1 > -5)
         {
 
@@ -287,7 +241,7 @@ void InvKinematics::findphi2phi3(double Xc, double Yc, double Zc, double phi1, s
             point1.push_back(phi1);                point2.push_back(BEup2);               point3.push_back(BEup3);
 
         }
-        else if (phi1 < -180 && phi1 < -175)
+        else if ((phi1 < -180 && phi1 < -175))
         {
             //normal cases   eg. -177 degrees
             point1.push_back(phi1);                point2.push_back(FEd2);                point3.push_back(FEd3);
@@ -299,19 +253,19 @@ void InvKinematics::findphi2phi3(double Xc, double Yc, double Zc, double phi1, s
             point1.push_back(phi1);                point2.push_back(BEup2);               point3.push_back(BEup3);
 
             //backward case 2    eg. +183 degrees
-            phi1 = phi1 - 180;
+            phi1 = phi1 + 180;
             point1.push_back(phi1);                point2.push_back(BEd2);                point3.push_back(BEd3);
             point1.push_back(phi1);                point2.push_back(BEup2);               point3.push_back(BEup3);
 
         }
-    
+
+        //cout << "\ntest 6.1 \n ";
         //cout << "\nsize of phi1, 2 and 3 arrays inside the findphi2phi3 function\n" << point1.size()<<"  " << point2.size()<<"  " << point3.size()<<"\n";
         //for (int f = 0; f < point1.size(); f++)
         //{
         //    cout << "\nprinting all phi1, phi2 and phi3 inside phi1, phi2 and phi3 function\n";
         //    cout << point1[f] << "   " << point2[f] << "   " << point3[f] << "\n";
         //}
-
 }
 
 std::tuple<double, double> InvKinematics::ForwardsElbowdown(double d1, double Zc)
@@ -338,6 +292,7 @@ std::tuple<double, double> InvKinematics::ForwardsElbowdown(double d1, double Zc
         b1 = acos(((d3 * d3) - (a * a) - (d2 * d2)) / (2 * a * d2)) * (180 / M_PI);
         //alpha 1 is a1
         //cout << "   b1\n" << b1;
+        if (b1 == 0) {}
         a1 = asin(Py / d3) * (180 / M_PI);
         //alpha 2 is a2
         //cout << "a1\n" << a1;
@@ -603,11 +558,13 @@ void InvKinematics::findphi4phi5phi6(double phi1, double phi2, double phi3, std:
 //
 ////equations - 1st set
 //
-////*****************calculate phi5 and check for singularity conditions*******************
 //
+
+
 
     double tempphi4 = atan2(-R36[2][1], -R36[2][0]) * (180 / M_PI);
     //cout << "\nphi4 is : "<<tempphi4<<"\n";
+
     while ((tempphi4 - 360) > -350) { tempphi4 -= 360; } //takes us to least possible value of tempphi4
     //cout << "\nlowest phi4 is : "<<tempphi4<<"\n";
 
@@ -618,6 +575,9 @@ void InvKinematics::findphi4phi5phi6(double phi1, double phi2, double phi3, std:
     {
 
         double tempphi5 = atan2(sqrt(1 - ((R36[2][2]) * (R36[2][2]))), R36[2][2]) * (180 / M_PI);
+        if (tempphi5 == 0)
+            tempphi5 = tempphi5 + 0.01;
+
        /*cout << "\nphi5 : " << tempphi5 << "\n";*/
         double tempphi6 = atan2(R36[1][2], R36[0][2]) * (180 / M_PI);
        /*cout << "\nphi6 : " << tempphi6 << "\n";*/
@@ -654,6 +614,9 @@ void InvKinematics::findphi4phi5phi6(double phi1, double phi2, double phi3, std:
     {
 
         double tempphi5 = atan2(-sqrt(1 - ((R36[2][2]) * (R36[2][2]))), R36[2][2]) * (180 / M_PI);
+        if (tempphi5 == 0)
+            tempphi5 = tempphi5 + 0.01;
+
         double tempphi6 = atan2(-R36[1][2], -R36[0][2]) * (180 / M_PI);
 
         while ((tempphi6 - 360) > -350) { tempphi6 -= 360; }  //takes us to least possible value of tempphi6
@@ -670,6 +633,7 @@ void InvKinematics::findphi4phi5phi6(double phi1, double phi2, double phi3, std:
         k++;
         tempphi4 += (double(360)*k);
     }
+
 }
 
 void InvKinematics::multiplymatrix(double M1[4][4], double M2[4][4], double(&(Result)))
@@ -728,4 +692,22 @@ void InvKinematics::printmatrix(double matrix[3][3])
 
         cout << "\n";
     }
+}
+void InvKinematics::degreetoradian(double & angle1, double & angle2, double &angle3, double &angle4, double &angle5, double &angle6)
+{
+    angle1 = angle1 * (M_PI / 180);
+    angle2 = angle2 * (M_PI / 180);
+    angle3 = angle3 * (M_PI / 180);
+    angle4 = angle4 * (M_PI / 180);
+    angle5 = angle5 * (M_PI / 180);
+    angle6 = angle6 * (M_PI / 180);
+}
+void InvKinematics::radiantodegrees(double& angle1, double& angle2, double& angle3, double& angle4, double& angle5, double& angle6)
+{
+    angle1 = angle1 * (180 / M_PI);
+    angle2 = angle2 * (180 / M_PI);
+    angle3 = angle3 * (180 / M_PI);
+    angle4 = angle4 * (180 / M_PI);
+    angle5 = angle5 * (180 / M_PI);
+    angle6 = angle6 * (180 / M_PI);
 }
