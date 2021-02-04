fda877c180aa9690facabd219249ccdc2fc63821
Sravani_Dhara <sravani.dhara@st.ovgu.de>
2021-02-04 14:15:40 +0000

    lin.h edited online with Bitbucket

--- ctrl/src/ctrl/pathplanner/lin/lin.h
+++ ctrl/src/ctrl/pathplanner/lin/lin.h
@@ -3,6 +3,11 @@
 
 #include <Configuration.h>
 #include <Trajectory.h>
+#include <../kinematics/inverse/inverse_kinematics.h>
+#include<../kinematics/direct/fw_kinematics.h>
+
+
+
 
 /**
  * This class implementes the lin movement from to given {@ref Configuration}, one for the start configuration and one
@@ -21,9 +26,13 @@ public:
      * @return {@ref Trajectoy} for the movement of the robot
      */
     Trajectory* get_lin_trajectoy(Configuration* _start_cfg, Configuration* _end_cfg);
-    double compute(double max_velo, double in_angle, double fin_angle);
+    double compute(double max_velo, double max_acc, double in_angle, double fin_angle, double time, double t_c, double t_fin);
     double new_speed(double in_angle, double fin_angle,double t_max);
+    //InvKinematics* Lin::inv_solution(InvKinematics* configurations, Configuration* _start_cfg, double in_phi1, double in_phi2, double in_phi3, double in_phi4, double in_phi5, double in_phi6);
+    InvKinematics* Lin::inv_solution(InvKinematics* solution, Configuration* _start_cfg, double in_phi1, double in_phi2, double in_phi3, double in_phi4, double in_phi5, double in_phi6, vector<Configuration*> configurations);
 };
+
+
 
 
 #endif //SDRI_CTRL2019_LIN_H
